8bd4af4bb834e1ac63b139f060819c30661aea5c
Harupiyo <harupiyo@gmail.com>
2025-11-29 11:42:05 +0900

    feat: ページコンテンツとインタラクションを更新

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
             e.preventDefault();
-            document.querySelector(this.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth'
-            });
+            const target = document.querySelector(this.getAttribute('href'));
+            if (target) {
+                target.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
         });
     });
 
